41cf6ecced2f5390f9827db948c5b46f24335bfc
fducourthial-cell <f.ducourthial@gmail.com>
2026-07-23 20:06:32 +0200

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,5 +1,5 @@
 // js/supabase.js
-const SUPABASE_URL = 'https://supabase.com/dashboard/project/rwvhdykxdedtefnpaygv/settings/api-keys/legacy';
+const SUPABASE_URL = 'https://rwvhdykxdedtefnpaygv.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ3dmhkeWt4ZGVkdGVmbnBheWd2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ4MjIzODIsImV4cCI6MjEwMDM5ODM4Mn0.X_0NvlZg0YjwC7ZpyY-nVidgoVv91jY1e-DT6zVqRGs';
 
 // Initialisation du client Supabase universel
